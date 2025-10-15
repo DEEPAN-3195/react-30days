f9b8ac9814f398ef971e68124a809232295c85c5
DEEPAN <108525282+DEEPAN-3195@users.noreply.github.com>
2025-10-15 13:11:28 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return <h1 className="text-3xl font-bold text-center text-blue-600">Hello React 30 Days 🚀</h1>;
+}
+
